98fcd5eeb6788afbe7fb815472e9c55ae1e54497
levirclark <levirclark@gmail.com>
2018-09-13 20:33:59 -0500

    Wrote comprehensive tests for given outline of item quality degradation. Added code to the updateQuality method to force Sulfurus to 80 quality. Refactored the update method into a grouping of smaller, more manageable methods. Added conjured items into update methods, with appropriate quality degradation. Added function to determine what is classified as a conjured item. This new function should be reviewed.

--- Java/com/gildedrose/GildedRose.java
+++ Java/com/gildedrose/GildedRose.java
@@ -2,6 +2,10 @@ package com.gildedrose;
 
 class GildedRose {
     Item[] items;
+    private final String SULFURUS = "Sulfuras, Hand of Ragnaros";
+    private final String BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
+    private final String AGED_BRIE = "Aged Brie";
+    private final String CONJURED_PREFIX = "Conjured";
 
     public GildedRose(Item[] items) {
         this.items = items;
@@ -9,54 +13,114 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+            
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+            items[i].sellIn = getUpdatedSellIn(items[i]);
+            items[i].quality = getUpdatedQuality(items[i]);
+            
         }
+    }
+    
+    private int getUpdatedQuality(Item item) {
+    	
+    	int updatedQuality = item.quality;
+    	boolean itemIsNormal = true;
+    	
+    	if(item.name.equals(SULFURUS)) {
+    		return getUpdatedSulfurusQuality(item);
+        }
+    	
+    	if(item.name.equals(AGED_BRIE)){
+    		itemIsNormal = false;
+    		updatedQuality = getUpdatedAgedBrieQuality(item);
+    	}
+    	
+    	if(item.name.equals(BACKSTAGE_PASS)) {
+    		itemIsNormal = false;
+    		updatedQuality = getUpdatedBackstagePassQuality(item);
+    	}
+    	
+    	if(itemIsConjured(item)) {
+    		itemIsNormal = false;
+    		updatedQuality = getUpdatedConjuredQuality(item);
+    	}
+    	
+    	//The item is a regular item
+    	
+    	if(itemIsNormal) {
+        	updatedQuality = getUpdatedGeneralItemQuality(item);
+    	}
+    	
+    	if(updatedQuality < 0) {
+    		updatedQuality = 0;
+    	}
+    	
+    	if(updatedQuality > 50) {
+    		updatedQuality = 50;
+    	}
+    	
+        return updatedQuality;
+    }
+    
+    private int getUpdatedSellIn(Item item) {
+    	if(item.name.equals(SULFURUS)) {
+    		return item.sellIn;
+    	}
+    	return item.sellIn - 1;
+    }
+    
+    private boolean itemIsConjured(Item item) {
+	//TODO This function returns true if the item is conjured. Currently any item with a name starting with "Conjured" counts. This needs to be verified with Allison.
+    	if(item.name == null) {
+    		return false;
+    	}
+    	return item.name.startsWith(CONJURED_PREFIX);
+    }
+    
+    private int getUpdatedSulfurusQuality(Item item) {
+    	return 80;
+    }
+    
+    private int getUpdatedAgedBrieQuality(Item item) {
+    	int updatedQuality = item.quality;
+    	updatedQuality++;
+		if(item.sellIn < 0) {
+			updatedQuality++;
+		}
+		return updatedQuality;
+    }
+    
+    private int getUpdatedBackstagePassQuality(Item item) {
+    	int updatedQuality = item.quality;
+    	int qualityIncrement = 1;
+		if(item.sellIn < 10) {
+			qualityIncrement++;
+		}
+		if(item.sellIn < 5) {
+			qualityIncrement++;
+		}
+		updatedQuality += qualityIncrement;
+		if(item.sellIn < 0) {
+			updatedQuality = 0;
+		}
+    	return updatedQuality;
+    }
+    
+    private int getUpdatedConjuredQuality(Item item) {
+    	int updatedQuality = item.quality;
+    	updatedQuality-= 2;
+    	if(item.sellIn < 0) {
+    		updatedQuality-= 2;
+    	}	
+    	return updatedQuality;
+    }
+    
+    private int getUpdatedGeneralItemQuality(Item item) {
+    	int updatedQuality = item.quality;
+    	updatedQuality--;
+    	if(item.sellIn < 0) {
+    		updatedQuality--;
+    	}	
+    	return updatedQuality;
     }
 }
--- Java/com/gildedrose/GildedRoseTest.java
+++ Java/com/gildedrose/GildedRoseTest.java
@@ -8,10 +8,89 @@ public class GildedRoseTest {
 
     @Test
     public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 20, 20) ,
+        		new Item("General Item", 20, 20), 
+        		new Item("General Old Item", 1, 20), 
+        		new Item("General Overdue Item", 0, 20), 
+        		new Item("Aged Brie", 0, 20),
+        		new Item("Aged Brie", 50, 20),
+        		new Item("Aged Brie", -5, 20),
+        		new Item("Aged Brie", 20, 20),
+        		new Item("Aged Brie", 20, 50),
+        		new Item("Backstage passes to a TAFKAL80ETC concert", 50, 20),
+        		new Item("Backstage passes to a TAFKAL80ETC concert", 11, 20),
+        		new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20),
+        		new Item("Backstage passes to a TAFKAL80ETC concert", 6, 20),
+        		new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20),
+        		new Item("Backstage passes to a TAFKAL80ETC concert", 1, 20),
+        		new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20),
+        		new Item("Backstage passes to a TAFKAL80ETC concert", 1, 50),
+        		new Item("Conjured Apple", 20, 20),
+        		new Item("Conjured Tablet", 1, 20),
+        		new Item("Conjured Piano", 0, 20)
+        		};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
+        assertEquals(80, app.items[0].quality);
+        assertEquals(20, app.items[0].sellIn);
+        assertEquals("General Item", app.items[1].name);
+        assertEquals(19, app.items[1].quality);
+        assertEquals(19, app.items[1].sellIn);
+        assertEquals("General Old Item", app.items[2].name);
+        assertEquals(19, app.items[2].quality);
+        assertEquals(0, app.items[2].sellIn);
+        assertEquals("General Overdue Item", app.items[3].name);
+        assertEquals(18, app.items[3].quality);
+        assertEquals(-1, app.items[3].sellIn);
+        assertEquals("Aged Brie", app.items[4].name);
+        assertEquals(22, app.items[4].quality);
+        assertEquals(-1, app.items[4].sellIn);
+        assertEquals("Aged Brie", app.items[5].name);
+        assertEquals(21, app.items[5].quality);
+        assertEquals(49, app.items[5].sellIn);
+        assertEquals("Aged Brie", app.items[6].name);
+        assertEquals(22, app.items[6].quality);
+        assertEquals(-6, app.items[6].sellIn);
+        assertEquals("Aged Brie", app.items[7].name);
+        assertEquals(21, app.items[7].quality);
+        assertEquals(19, app.items[7].sellIn);
+        assertEquals("Aged Brie", app.items[8].name);
+        assertEquals(50, app.items[8].quality);
+        assertEquals(19, app.items[8].sellIn);
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[9].name);
+        assertEquals(21, app.items[9].quality);
+        assertEquals(49, app.items[9].sellIn);
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[10].name);
+        assertEquals(21, app.items[10].quality);
+        assertEquals(10, app.items[10].sellIn);
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[11].name);
+        assertEquals(22, app.items[11].quality);
+        assertEquals(9, app.items[11].sellIn);
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[12].name);
+        assertEquals(22, app.items[12].quality);
+        assertEquals(5, app.items[12].sellIn);
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[13].name);
+        assertEquals(23, app.items[13].quality);
+        assertEquals(4, app.items[13].sellIn);
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[14].name);
+        assertEquals(23, app.items[14].quality);
+        assertEquals(0, app.items[14].sellIn);
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[15].name);
+        assertEquals(0, app.items[15].quality);
+        assertEquals(-1, app.items[15].sellIn);
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[16].name);
+        assertEquals(50, app.items[16].quality);
+        assertEquals(0, app.items[16].sellIn);
+        assertEquals("Conjured Apple", app.items[17].name);
+        assertEquals(18, app.items[17].quality);
+        assertEquals(19, app.items[17].sellIn);
+        assertEquals("Conjured Tablet", app.items[18].name);
+        assertEquals(18, app.items[18].quality);
+        assertEquals(0, app.items[18].sellIn);
+        assertEquals("Conjured Piano", app.items[19].name);
+        assertEquals(16, app.items[19].quality);
+        assertEquals(-1, app.items[19].sellIn);
     }
 
 }
